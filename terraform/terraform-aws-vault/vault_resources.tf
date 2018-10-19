3d63f81e7cb23ad369668378eafcfef2521444ee
Chuck Hilyard <chuck.hilyard@reachlocal.com>
2018-10-19 14:37:35 -0700

    ldap

---
+++ terraform/terraform-aws-vault/vault_resources.tf
@@ -0,0 +1,11 @@
+
+resource "vault_ldap_auth_backend" "ldap_backend" {
+  path        = "${vault_auth_backend.ldap.path}"
+  url         = "ldap://auth.wh.reachlocal.com"
+  userdn      = "OU=People,DC=reachlocal,DC=com"
+  userattr    = "sAMAccountName"
+  upndomain   = "EXAMPLE.ORG"
+  discoverdn  = false
+  groupdn     = "OU=Group,DC=reachlocal,DC=com"
+  groupfilter = "(cn=eng)"
+}
