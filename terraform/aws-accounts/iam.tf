4a21f4950857d1c95d615935f068697e9b5f98d1
Chuck Hilyard <chuck.hilyard@reachlocal.com>
2018-10-25 14:55:21 -0700

    updates

---
+++ terraform/aws-accounts/iam.tf
@@ -0,0 +1,48 @@
+
+resource "aws_iam_user" "jenkins" {
+  name = "media.jenkins"
+}
+
+resource "aws_iam_user" "terraform" {
+  name = "media.terraform"
+}
+
+resource "aws_iam_user_policy" "base_camp_user_policy_jenkins" {
+  name = "base-camp-user-policy-jenkins"
+  user = "${aws_iam_user.jenkins.name}"
+  policy = << HereDoc
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": [
+        "ec2:*",
+        "s3:*"
+      ],
+      "Effect": "Allow",
+      "Resource": "*"
+    }
+  ]
+}
+HereDoc
+}
+
+resource "aws_iam_user_policy" "base_camp_user_policy_terraform" {
+  name = "base-camp-user-policy-terraform"
+  user = "${aws_iam_user.terraform.name}"
+  policy = << HereDoc
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": [
+        "ec2:*",
+        "s3:*"
+      ],
+      "Effect": "Allow",
+      "Resource": "*"
+    }
+  ]
+}
+HereDoc
+}
