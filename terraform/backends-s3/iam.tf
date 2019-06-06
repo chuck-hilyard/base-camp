489e6704d4d54bf24dc9b31df31d294ffa6d68c2
Chuck Hilyard <chuck.hilyard@reachlocal.com>
2019-06-06 09:06:26 -0700

    iam stuff

--- terraform/backends-s3/iam.tf
+++ terraform/backends-s3/iam.tf
@@ -1,3 +1,9 @@
+#arn:aws:iam::762858336698:role/service-role/s3crr_role_for_media-team-secrets_to_media-team-secrets-backup
+resource "aws_iam_service_linked_role" "secrets_replication_service_linked_role" {
+  description      = "media-team-secrets-replication-service-role"
+  aws_service_name = "s3.amazonaws.com"
+}
+
 resource "aws_iam_role" "secrets_replication" {
   name = "media-team-secrets-replication"
   assume_role_policy = <<EOF
@@ -15,4 +21,38 @@ resource "aws_iam_role" "secrets_replication" {
   ]
 }
 EOF
+}
+
+
+resource "aws_iam_policy" "secrets_replication" { 
+  name = "media-team-secrets-replication"
+  path = "/"
+  policy = <<HereDoc
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Action": [
+                "s3:Get*",
+                "s3:ListBucket"
+            ],
+            "Effect": "Allow",
+            "Resource": [
+                "arn:aws:s3:::media-team-secrets",
+                "arn:aws:s3:::media-team-secrets/*"
+            ]
+        },
+        {
+            "Action": [
+                "s3:ReplicateObject",
+                "s3:ReplicateDelete",
+                "s3:ReplicateTags",
+                "s3:GetObjectVersionTagging"
+            ],
+            "Effect": "Allow",
+            "Resource": "arn:aws:s3:::media-team-secrets-backup/*"
+        }
+    ]
+}
+HereDoc
 }
