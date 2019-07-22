6c4a58e517e77f55e7564f72d584b5d88e578fd7
Chuck Hilyard <chuck.hilyard@reachlocal.com>
2019-07-22 15:24:08 -0700

    public access lockdown

--- terraform/backends-s3/s3.tf
+++ terraform/backends-s3/s3.tf
@@ -103,6 +103,8 @@ resource "aws_s3_bucket_public_access_block" "secrets_source" {
   bucket = "${aws_s3_bucket.secrets_source.id}"
   block_public_acls   = true
   block_public_policy = true
+  ignore_public_acls  = true
+  restrict_public_buckets = true
 }
 
 resource "aws_s3_bucket" "secrets_source" {
