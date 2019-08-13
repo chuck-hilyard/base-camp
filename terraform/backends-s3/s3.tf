2ee28df50b817ccd545741a13c27a5e7b3f9eaf9
Chuck Hilyard <chuck.hilyard@reachlocal.com>
2019-08-13 16:23:36 -0700

    breaking out media team secret buckets

--- terraform/backends-s3/s3.tf
+++ terraform/backends-s3/s3.tf
@@ -99,16 +99,16 @@ HereDoc
 
 //MEDIA TEAM SECRETS
 
-resource "aws_s3_bucket_public_access_block" "secrets_source" {
-  bucket = "${aws_s3_bucket.secrets_source.id}"
+resource "aws_s3_bucket_public_access_block" "secrets_source_user" {
+  bucket = "${aws_s3_bucket.secrets_source_user.id}"
   block_public_acls   = true
   block_public_policy = true
   ignore_public_acls  = true
   restrict_public_buckets = true
 }
 
-resource "aws_s3_bucket" "secrets_source" {
-  bucket        = "media-team-secrets"
+resource "aws_s3_bucket" "secrets_source_user" {
+  bucket        = "media-team-secrets-user"
   acl           = "private"
   force_destroy = false
   #logging       {}
@@ -118,7 +118,52 @@ resource "aws_s3_bucket" "secrets_source" {
     rules {
       status = "Enabled"
       destination {
-        bucket = "${aws_s3_bucket.secrets_destination.arn}"
+        bucket = "${aws_s3_bucket.secrets_destination_user.arn}"
+      }
+    }
+  }
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
+  versioning {
+    enabled = true
+  }
+  policy = <<HereDoc
+{
+    "Id": "Policy1539969783840",
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Sid": "Stmt1539969782489",
+            "Action": "s3:*",
+            "Effect": "Deny",
+            "Resource": [
+                "arn:aws:s3:::media-team-secrets-user/*"
+            ],
+            "NotPrincipal": ["arn:aws:iam::762858336698:role/AdminFA", "arn:aws:iam::762858336698:user/media.terraform", "arn:aws:iam::762858336698:user/vault.secrets.user"]
+        }
+    ]
+}
+HereDoc
+}
+
+/*
+resource "aws_s3_bucket" "secrets_source_preprod" {
+  bucket        = "media-team-secrets-user"
+  acl           = "private"
+  force_destroy = false
+  #logging       {}
+  region        = "us-west-2"
+  replication_configuration {
+    role = "${aws_iam_role.secrets_replication.arn}"
+    rules {
+      status = "Enabled"
+      destination {
+        bucket = "${aws_s3_bucket.secrets_destination_preprod.arn}"
       }
     }
   }
@@ -144,11 +189,6 @@ resource "aws_s3_bucket" "secrets_source" {
             "Resource": [
                 "arn:aws:s3:::media-team-secrets/*"
             ],
-            "Condition": {
-                "NotIpAddress": {
-                    "aws:SourceIp": ["52.9.42.1/24", "10.0.0.0/8"]
-                }
-            },
             "Principal": "*"
         },
         {
@@ -210,8 +250,111 @@ resource "aws_s3_bucket" "secrets_source" {
 }
 HereDoc
 }
+*/
+
+/*
+resource "aws_s3_bucket" "secrets_source_prod" {
+  bucket        = "media-team-secrets-user"
+  acl           = "private"
+  force_destroy = false
+  #logging       {}
+  region        = "us-west-2"
+  replication_configuration {
+    role = "${aws_iam_role.secrets_replication.arn}"
+    rules {
+      status = "Enabled"
+      destination {
+        bucket = "${aws_s3_bucket.secrets_destination_prod.arn}"
+      }
+    }
+  }
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
+  versioning {
+    enabled = true
+  }
+  policy = <<HereDoc
+{
+    "Id": "Policy1539969783840",
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Sid": "Stmt1539969782489",
+            "Action": "s3:*",
+            "Effect": "Deny",
+            "Resource": [
+                "arn:aws:s3:::media-team-secrets/*"
+            ],
+            "Principal": "*"
+        },
+        {
+            "Sid": "Stmt1559834358936",
+            "Action": "s3:*",
+            "Effect": "Allow",
+            "Resource": "arn:aws:s3:::media-team-secrets",
+            "Principal": {
+                "AWS": [
+                    "arn:aws:iam::762858336698:user/media.vault",
+                    "${aws_iam_role.secrets_replication.arn}",
+                    "arn:aws:iam::762858336698:role/AdminFA"
+                ]
+            }
+        },
+        {
+            "Sid": "Stmt1559834358936",
+            "Effect": "Deny",
+            "NotPrincipal": {
+                "AWS": [
+                    "arn:aws:iam::762858336698:role/AdminFA",
+                    "arn:aws:iam::762858336698:user/media.vault",
+                    "arn:aws:iam::762858336698:role/media-team-secrets-replication",
+                    "arn:aws:iam::762858336698:user/test"
+                ]
+            },
+            "Action": "s3:*",
+            "Resource": "arn:aws:s3:::media-team-secrets/dev/*"
+        },
+        {
+            "Sid": "Stmt1559834358937",
+            "Effect": "Deny",
+            "NotPrincipal": {
+                "AWS": [
+                    "arn:aws:iam::762858336698:role/AdminFA",
+                    "arn:aws:iam::762858336698:user/media.vault",
+                    "arn:aws:iam::762858336698:role/media-team-secrets-replication",
+                    "arn:aws:iam::762858336698:user/test"
+                ]
+            },
+            "Action": "s3:*",
+            "Resource": "arn:aws:s3:::media-team-secrets/qa/*"
+        },
+        {
+            "Sid": "Stmt1559834358938",
+            "Effect": "Deny",
+            "NotPrincipal": {
+                "AWS": [
+                    "arn:aws:iam::762858336698:role/AdminFA",
+                    "arn:aws:iam::762858336698:user/media.vault",
+                    "arn:aws:iam::762858336698:role/media-team-secrets-replication",
+                    "arn:aws:iam::762858336698:user/test"
+                ]
+            },
+            "Action": "s3:*",
+            "Resource": "arn:aws:s3:::media-team-secrets/prod/*"
+        }
+    ]
+}
+HereDoc
+}
+*/
 
 
+/* deactivated, for now
 resource "aws_s3_bucket" "secrets_destination" {
   provider      = "aws.can"
   bucket        = "media-team-secrets-backup"
@@ -234,11 +377,6 @@ resource "aws_s3_bucket" "secrets_destination" {
             "Resource": [
                 "arn:aws:s3:::media-team-secrets-backup/*"
             ],
-            "Condition": {
-                "NotIpAddress": {
-                    "aws:SourceIp": "52.9.42.1/24"
-                }
-            },
             "Principal": "*"
         },
         {
@@ -258,5 +396,5 @@ resource "aws_s3_bucket" "secrets_destination" {
 }
 HereDoc
 }
-
+*/ 
 
